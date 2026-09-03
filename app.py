7dc33b209da92ed2e007bd4f38776f6277c0df59
zageabb <zageabb@users.noreply.github.com>
2026-09-03 13:10:04 +0100

    Enable Playwright fallback in web research

--- app.py
+++ app.py
@@ -7,10 +7,13 @@ from io import BytesIO
 
 from flask import Flask, jsonify, render_template, request, send_file
 
+from browser_fetch import install_browser_fallback
 from document_extraction import clean_documents, document_context, extract_upload
 from search import JOBS, list_models, start_job
 from settings_store import PROMPTS, get_settings, save_prompts, save_settings
 
+
+install_browser_fallback()
 
 app = Flask(__name__)
 app.config["MAX_CONTENT_LENGTH"] = 30_000_000
